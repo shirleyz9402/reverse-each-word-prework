f8e41a9dc87bcfc296a4a13a9905d644a260a93b
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-22 22:43:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
   reversed = []
   words = sentence1.split(" ")
-  words.each do |word|
+  words.collect do |word|
     reversed.push(word.reverse)
   end 
   reversed.join(" ")
